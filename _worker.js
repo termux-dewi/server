7823b54daf8de08aab0da51a766beb01b9c2463d
del54 <64617020+del54@users.noreply.github.com>
2024-12-27 18:04:05 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "simokasu.web.id"; // Ganti dengan domain utama kalian
-const serviceName = "simok"; // Ganti dengan nama workers kalian
+const rootDomain = ""; // Ganti dengan domain utama kalian
+const serviceName = ""; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
